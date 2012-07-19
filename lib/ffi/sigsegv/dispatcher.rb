94e696a53f915f47291ff07748ddd222dfb372ba
Postmodern <postmodern.mod3@gmail.com>
2012-07-18 23:00:57 -0700

    Have SigSEGV::Dispatcher#initialize call sigsegv_init.

--- lib/ffi/sigsegv/dispatcher.rb
+++ lib/ffi/sigsegv/dispatcher.rb
@@ -1,3 +1,5 @@
+require 'ffi/sigsegv/sigsegv'
+
 require 'ffi'
 
 module FFI
@@ -5,6 +7,24 @@ module FFI
     class Dispatcher < FFI::Struct
 
       layout :tree, :pointer
+
+      #
+      # Initializes a new SigSEGV dispatcher.
+      #
+      # @param [FFI::Pointer] ptr
+      #   Optional pointer to an existing dispatcher.
+      #
+      # @note
+      #   If no `ptr` is given, the dispatcher will be initialized by calling
+      #   `sigsegv_init`.
+      #
+      def initialize(ptr=nil)
+        if ptr
+          super(ptr)
+        else
+          SigSEGV.sigsegv_init(super())
+        end
+      end
 
     end
   end
