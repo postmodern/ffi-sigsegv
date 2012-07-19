04b83080b62fa81b8911d3ea8527395bca8b0c40
Postmodern <postmodern.mod3@gmail.com>
2012-07-18 23:50:14 -0700

    Added FFI::SigSEGV::StackOverflow.

--- lib/ffi/sigsegv.rb
+++ lib/ffi/sigsegv.rb
@@ -1,2 +1,4 @@
 require 'ffi/sigsegv/sigsegv'
+require 'ffi/sigsegv/dispatcher'
+require 'ffi/sigsegv/stack_overflow'
 require 'ffi/sigsegv/version'
---
+++ lib/ffi/sigsegv/stack_overflow.rb
@@ -0,0 +1,49 @@
+require 'ffi/sigsegv/sigsegv'
+
+module FFI
+  module SigSEGV
+    module StackOverflow
+
+      SIGSTKSZ = (16 * 1024)
+
+      @handler     = nil
+      @extra_stack = nil
+
+      #
+      # Installs a Stack Overflow handler.
+      #
+      # @param [Integer] extra_stack_size
+      #   The size of the extra stack space, to be used when the handler is
+      #   called.
+      #
+      # @return [true]
+      #   Success.
+      #
+      # @raise
+      #   The system does not support catching stack overflows.
+      #
+      def self.install(extra_stack_size=SIGSTKSZ,&block)
+        @handler     = block
+        @extra_stack = FFI::Buffer.new(extra_stack_size)
+
+        unless SigSEGV.stackoverflow_install_handler(@extra_stack,extra_stack_size,block) == 0
+          raise("system does not support catching stack overflows")
+        end
+
+        return true
+      end
+
+      #
+      # Uninstalls the stack overflow handler.
+      #
+      def self.deinstall
+        SigSEGV.stackoverflow_deinstall_handler
+
+        @handler     = nil
+        @extra_stack = nil
+        return true
+      end
+
+    end
+  end
+end
